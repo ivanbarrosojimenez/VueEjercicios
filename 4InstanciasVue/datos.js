39e4c341b0dfbfb76cce6f27351f83691bd7a444
ibarrosoj <ibarrosj@everis.com>
2020-11-14 18:02:41 +0100

    Ejercicio 4

---
+++ 4InstanciasVue/datos.js
@@ -0,0 +1,22 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        mensaje:'Mi blog',
+        titulo1: 'Titulo 1',
+        post1: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        titulo2: 'Titulo 2',
+        post2: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+        titulo3: 'Titulo 3',
+        post3: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    }
+});
+
+const app2 = new Vue({
+    el: '#app2',
+    data: {
+        mensaje:'Listado de elementos',
+        titulo1: '11/11/2020 20:38',
+        titulo2: '12/11/2020 10:26',
+        titulo3: '13/11/2020 15:45',
+    }
+});
