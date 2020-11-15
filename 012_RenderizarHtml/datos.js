1594307eebef5382b66c138298c02ee3aeaaf88e
ibarrosoj <ibarrosj@everis.com>
2020-11-15 12:49:20 +0100

    11 renderizar html

---
+++ 012_RenderizarHtml/datos.js
@@ -0,0 +1,7 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo:'Renderizar html',
+        contenidoHtml: '<div><h2>Ejemplo de renderizado html</h2>Enlace a <a href="https://www.google.com">Google</a></div>'
+    }
+});
