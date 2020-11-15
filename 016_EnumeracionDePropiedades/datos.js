6124e0526935501f772890fd1dcb00a45a5a1144
ibarrosoj <ibarrosj@everis.com>
2020-11-15 17:22:12 +0100

    16 enumeración de propiedades

---
+++ 016_EnumeracionDePropiedades/datos.js
@@ -0,0 +1,12 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        titulo:'Enumeración de propiedades'
+        ,post:{
+            titulo: 'Nuevo articulo'
+            ,fecha: '22/10/2021'
+            ,autor: 'Iván Barroso'
+            ,contenido: 'Contenido del articulo'
+        }
+    }
+});
